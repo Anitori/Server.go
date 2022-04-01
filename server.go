29cae082925cfbd256f2d0633fdf3336ffc99189
Emanuel Rodriguez Anitori <88413740+Anitori@users.noreply.github.com>
2022-03-31 23:57:50 -0300

    Tests

--- server.go
+++ server.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"net/http"
+	// "fmt"
 )
 
 func main() {
 
 	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.Handle("/c", http.StripPrefix("/c", fs))
 
 	http.ListenAndServe(":8080", nil)
+	// handler()
 }
+
+// func handler(w http.ResponseWriter, r *http.Request) {
+// fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+// }
